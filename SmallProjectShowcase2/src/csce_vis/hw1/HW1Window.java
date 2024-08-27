7a3af58737f5d29cc2c75f97f7f212a310842260
Andrew Li <andwerpooni@gmail.com>
2024-08-27 13:58:36 -0500

    added triangle collider

--- SmallProjectShowcase2/src/csce_vis/hw1/HW1Window.java
+++ SmallProjectShowcase2/src/csce_vis/hw1/HW1Window.java
@@ -46,10 +46,16 @@ public class HW1Window extends Window {
 	// - handle physics updates in compute shader (prob not worth it)
 	// - ball size variance when generating. 
 	// - allow to change box dimensions along each axis seperately. 
+	// - fun stuff
+	//   - first player movement on the ground
+	//   - boss fight? have another big ball fly around and pelt the player with balls
+	//   - basketball hoop. use a plane to detect if you score
+	//   - general triangle collider to make obstacles on the ground
 
 	private PlayerInputController pc;
 
 	private ArrayList<Sphere> spheres;
+	private ArrayList<Triangle> triangles;
 
 	private RenderScreen renderScreen;
 
@@ -70,6 +76,7 @@ public class HW1Window extends Window {
 		this.pc.setAcceptPlayerInputs(false);
 
 		this.spheres = new ArrayList<>();
+		this.triangles = new ArrayList<>();
 
 		this.renderScreen = new RenderScreen();
 
@@ -109,6 +116,11 @@ public class HW1Window extends Window {
 
 		List<Triangle> tris = Arrays.asList(t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11);
 		this.renderScreen.setTriangles(tris);
+
+		this.triangles.clear();
+		for (Triangle t : tris) {
+			this.triangles.add(t);
+		}
 
 		this.renderScreen.setBoxSize(size);
 	}
@@ -177,20 +189,11 @@ public class HW1Window extends Window {
 		tangent_norm.normalize();
 
 		float tangent_vel = tangent_norm.dot(relative_vel);
-		float tangent_impulse_scalar = tangent_vel / inv_mass_sum;	//static friction. should 'match speed' with other surface
-//		float tangent_impulse_scalar = tangent_vel / inv_mass_sum;
-
-		//		System.err.println("TANGENT NORM : " + tangent_norm + " " + tangent_norm.dot(normal));
-		//		System.err.println("REL VEL : " + relative_vel);
-		//		System.err.println("TANGENT VEL : " + tangent_vel);
-		//		System.err.println("CONTACT VEC : " + contact_vec);
-		//		System.err.println("S POS : " + s.pos);
-		//		System.err.println("ANG VEL : " + s.ang_vel);
+		float tangent_impulse_scalar = tangent_vel / inv_mass_sum; //static friction. should 'match speed' with other surface
 
 		if (tangent_impulse_scalar > normal_impulse_scalar * settings.staticFriction) {
 			//if the force is too great for static friction, switch to dynamic friction
 			tangent_impulse_scalar = settings.dynamicFriction * normal_impulse_scalar;
-//			tangent_impulse_scalar *= settings.dynamicFriction;
 		}
 
 		//apply tangent impulse
@@ -198,6 +201,8 @@ public class HW1Window extends Window {
 	}
 
 	private void physicsStep() {
+		//maybe apply acceleration changes directly instead of buffering them?
+		//will help resolve cases where two objects are pushing in the same direction. 
 		Vec3[] accel = new Vec3[this.spheres.size()];
 		Vec3[] pen_correct = new Vec3[this.spheres.size()];
 		Vec3[] ang_accel = new Vec3[this.spheres.size()];
@@ -207,7 +212,47 @@ public class HW1Window extends Window {
 			ang_accel[i] = new Vec3(0);
 		}
 
-		//collisions
+		//sphere vs. triangle
+		for (int i = 0; i < this.spheres.size(); i++) {
+			for (int j = 0; j < this.triangles.size(); j++) {
+				Sphere s = this.spheres.get(i);
+				Triangle t = this.triangles.get(j);
+
+				//find closest point on triangle to sphere center. 
+				Vec3 deep_pt = MathUtils.sphere_triangleIntersect(s.pos, s.radius, t.p0, t.p1, t.p2);
+
+				if (deep_pt == null) { //no intersection
+					continue;
+				}
+
+				//force coll_pt to be on the surface of the sphere
+				Vec3 coll_pt = new Vec3(deep_pt);
+				coll_pt.subi(s.pos);
+				if (coll_pt.lengthSq() < 1e-9) {
+					coll_pt = new Vec3(1, 0, 0);
+				}
+				coll_pt.setLength(s.radius);
+				coll_pt.addi(s.pos);
+
+				//penetration correction
+				pen_correct[i].addi(deep_pt.sub(coll_pt));
+
+				//resolve collision
+				Vec3 relative_vel = new Vec3(0);
+				relative_vel.subi(s.vel);
+				relative_vel.subi(calcSurfaceVel(s.ang_vel, s.pos, coll_pt));
+
+				this.handleCollision(accel, ang_accel, i, s, coll_pt, relative_vel, 0);
+
+				//hack to avoid double collisions: immediately apply accelerations
+				s.vel.addi(accel[i]);
+				s.ang_vel.addi(ang_accel[i]);
+				accel[i].set(0, 0, 0);
+				ang_accel[i].set(0, 0, 0);
+			}
+		}
+
+		//sphere vs. sphere 
 		for (int i = 0; i < this.spheres.size(); i++) {
 			for (int j = 0; j < this.spheres.size(); j++) {
 				if (i == j) {
@@ -227,12 +272,9 @@ public class HW1Window extends Window {
 				ba_norm.normalize();
 
 				//need to correct penetration. Move two balls equally
-				if (i < j) {
-					float pen = (sa.radius + sb.radius) - ba.length();
-					pen = Math.max(0, pen - settings.penetrationThreshold);
-					pen_correct[i].addi(ba_norm.mul(pen / 2.0f));
-					pen_correct[j].subi(ba_norm.mul(pen / 2.0f));
-				}
+				float pen = (sa.radius + sb.radius) - ba.length();
+				pen = Math.max(0, pen - settings.penetrationThreshold);
+				pen_correct[i].addi(ba_norm.mul(pen / 2.0f));
 
 				//compute collision point
 				Vec3 coll_pt = sa.pos.add(sb.pos).mul(0.5f);
@@ -247,60 +289,71 @@ public class HW1Window extends Window {
 				this.handleCollision(accel, ang_accel, i, sa, coll_pt, relative_vel, 1.0f / sb.mass);
 			}
 		}
-		
+
 		//basics
 		for (int i = 0; i < this.spheres.size(); i++) {
 			Sphere s = this.spheres.get(i);
 
-			//wall collision
-			if ((s.pos.x - s.radius < -settings.boxSize && s.vel.x < 0) || (s.pos.x + s.radius > settings.boxSize && s.vel.x > 0)) { //horizontal
-				Vec3 coll_pt = new Vec3(s.pos);
-				if (s.pos.x - s.radius < -settings.boxSize) {
-					pen_correct[i].addi(new Vec3(-settings.boxSize - (s.pos.x - s.radius), 0, 0));
-					coll_pt.x -= s.radius;
-				}
-				else {
-					pen_correct[i].addi(new Vec3(settings.boxSize - (s.pos.x + s.radius), 0, 0));
-					coll_pt.x += s.radius;
-				}
-				Vec3 relative_vel = new Vec3(0);
-				relative_vel.subi(s.vel);
-				relative_vel.subi(calcSurfaceVel(s.ang_vel, s.pos, coll_pt));
-				this.handleCollision(accel, ang_accel, i, s, coll_pt, relative_vel, 0);
-			}
-			if ((s.pos.y - s.radius < -settings.boxSize && s.vel.y < 0) || (s.pos.y + s.radius > settings.boxSize && s.vel.y > 0)) { //vertical
-				Vec3 coll_pt = new Vec3(s.pos);
-				if (s.pos.y - s.radius < -settings.boxSize) {
-					pen_correct[i].addi(new Vec3(0, -settings.boxSize - (s.pos.y - s.radius), 0));
-					coll_pt.y -= s.radius;
-				}
-				else {
-					pen_correct[i].addi(new Vec3(0, settings.boxSize - (s.pos.y + s.radius), 0));
-					coll_pt.y += s.radius;
-				}
-				Vec3 relative_vel = new Vec3(0);
-				relative_vel.subi(s.vel);
-				relative_vel.subi(calcSurfaceVel(s.ang_vel, s.pos, coll_pt));
-				this.handleCollision(accel, ang_accel, i, s, coll_pt, relative_vel, 0);
-			}
-			if ((s.pos.z - s.radius < -settings.boxSize && s.vel.z < 0) || (s.pos.z + s.radius > settings.boxSize && s.vel.z > 0)) { //depth?
-				Vec3 coll_pt = new Vec3(s.pos);
-				if (s.pos.z - s.radius < -settings.boxSize) {
-					pen_correct[i].addi(new Vec3(0, 0, -settings.boxSize - (s.pos.z - s.radius)));
-					coll_pt.z -= s.radius;
-				}
-				else {
-					pen_correct[i].addi(new Vec3(0, 0, settings.boxSize - (s.pos.z + s.radius)));
-					coll_pt.z += s.radius;
-				}
-				Vec3 relative_vel = new Vec3(0);
-				relative_vel.subi(s.vel);
-				relative_vel.subi(calcSurfaceVel(s.ang_vel, s.pos, coll_pt));
-				this.handleCollision(accel, ang_accel, i, s, coll_pt, relative_vel, 0);
-			}
+			//			//wall collision
+			//			if ((s.pos.x - s.radius < -settings.boxSize && s.vel.x < 0) || (s.pos.x + s.radius > settings.boxSize && s.vel.x > 0)) { //horizontal
+			//				Vec3 coll_pt = new Vec3(s.pos);
+			//				if (s.pos.x - s.radius < -settings.boxSize) {
+			//					pen_correct[i].addi(new Vec3(-settings.boxSize - (s.pos.x - s.radius), 0, 0));
+			//					coll_pt.x -= s.radius;
+			//				}
+			//				else {
+			//					pen_correct[i].addi(new Vec3(settings.boxSize - (s.pos.x + s.radius), 0, 0));
+			//					coll_pt.x += s.radius;
+			//				}
+			//				Vec3 relative_vel = new Vec3(0);
+			//				relative_vel.subi(s.vel);
+			//				relative_vel.subi(calcSurfaceVel(s.ang_vel, s.pos, coll_pt));
+			//				this.handleCollision(accel, ang_accel, i, s, coll_pt, relative_vel, 0);
+			//			}
+			//			if ((s.pos.y - s.radius < -settings.boxSize && s.vel.y < 0) || (s.pos.y + s.radius > settings.boxSize && s.vel.y > 0)) { //vertical
+			//				Vec3 coll_pt = new Vec3(s.pos);
+			//				if (s.pos.y - s.radius < -settings.boxSize) {
+			//					pen_correct[i].addi(new Vec3(0, -settings.boxSize - (s.pos.y - s.radius), 0));
+			//					coll_pt.y -= s.radius;
+			//				}
+			//				else {
+			//					pen_correct[i].addi(new Vec3(0, settings.boxSize - (s.pos.y + s.radius), 0));
+			//					coll_pt.y += s.radius;
+			//				}
+			//				Vec3 relative_vel = new Vec3(0);
+			//				relative_vel.subi(s.vel);
+			//				relative_vel.subi(calcSurfaceVel(s.ang_vel, s.pos, coll_pt));
+			//				this.handleCollision(accel, ang_accel, i, s, coll_pt, relative_vel, 0);
+			//			}
+			//			if ((s.pos.z - s.radius < -settings.boxSize && s.vel.z < 0) || (s.pos.z + s.radius > settings.boxSize && s.vel.z > 0)) { //depth?
+			//				Vec3 coll_pt = new Vec3(s.pos);
+			//				if (s.pos.z - s.radius < -settings.boxSize) {
+			//					pen_correct[i].addi(new Vec3(0, 0, -settings.boxSize - (s.pos.z - s.radius)));
+			//					coll_pt.z -= s.radius;
+			//				}
+			//				else {
+			//					pen_correct[i].addi(new Vec3(0, 0, settings.boxSize - (s.pos.z + s.radius)));
+			//					coll_pt.z += s.radius;
+			//				}
+			//				Vec3 relative_vel = new Vec3(0);
+			//				relative_vel.subi(s.vel);
+			//				relative_vel.subi(calcSurfaceVel(s.ang_vel, s.pos, coll_pt));
+			//				this.handleCollision(accel, ang_accel, i, s, coll_pt, relative_vel, 0);
+			//			}
 
 			//gravity
 			accel[i].addi(settings.gravity.mul(settings.timeStep));
+
+			//air resistance
+			Vec3 rel_air_dir = settings.wind.sub(s.vel);
+			float air_vel_sq = rel_air_dir.lengthSq();
+			float cross_section_area = (float) (Math.PI * s.radius * s.radius);
+			if (air_vel_sq > 1e-6) {
+				rel_air_dir.normalize();
+				Vec3 air_res_force = rel_air_dir.mul(air_vel_sq * cross_section_area).div(s.mass).mul(settings.airResistanceCoeff);
+				accel[i].addi(air_res_force.mul(settings.timeStep));
+			}
+
 		}
 
 		//assign values of next step
@@ -442,16 +495,34 @@ public class HW1Window extends Window {
 		public Vec3 gravity = new Vec3(0, -300, 0);
 		public int invTimeStep = 60;
 		public float timeStep = 1.0f / this.invTimeStep;
-		public float coeffRestitution = 0.9f;
+		public float coeffRestitution = 0.3f;
 		public float penetrationThreshold = 0.05f; //threshold before which we do penetration correction. 
 		public float dynamicFriction = 0f;
 		public float staticFriction = 0f;
 		public boolean doAngVelDamping = true;
 		public boolean renderSphereTexture = false;
 		public int ballCount = 1;
+		public Vec3 wind = new Vec3(0);
+		public float airResistanceCoeff = 0;
 
 		public SimulationSettings() {
 			setBoxSize(this.boxSize);
+		}
+
+		public float getAirResistanceCoeff() {
+			return airResistanceCoeff;
+		}
+
+		public void setAirResistanceCoeff(float airResistanceCoeff) {
+			this.airResistanceCoeff = airResistanceCoeff;
+		}
+
+		public Vec3 getWind() {
+			return wind;
+		}
+
+		public void setWind(Vec3 wind) {
+			this.wind = wind;
 		}
 
 		public int getInvTimeStep() {
